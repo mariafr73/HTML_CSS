0a91202e2ab722cb2d78e33a1792c3f37a1ebd57
Maria Francisca <pessoamaria391@gmail.com>
2025-09-07 12:58:10 -0300

    Delete HTML_CSS_inicio/Python directory
    Removendo a pasta PYTHON

--- HTML_CSS_inicio/Python/estudos python/aula 01.py
+++
@@ -1,5 +0,0 @@
-print("Hello, World!")
-
-#Desafio 01
-
-print("Olá, eu sou Maria Francisca. Estou\n aprendendo a programar, e já\n consigo rodas meus primeiros\n programas em Python.")
--- HTML_CSS_inicio/Python/estudos python/aula 02.py
+++
@@ -1,7 +0,0 @@
-print("Hello, ")
-print("World!")
-print("Mensagem 1")
-print("Mensagem 2")
-print("Mensagem 3")
-print("Mensagem 4")
-print()
